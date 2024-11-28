deb08430fd730ac24868c16d79d1bf08fc72dfa3
Prashant <nandipati.prashant1509@gmail.com>
2024-11-28 19:20:30 +0000

    add kubernetes checks

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -7,22 +7,29 @@ import (
 	"github.com/charmbracelet/log"
 	"github.com/itspacchu/kubeloki/cmd/kubeapi"
 	"github.com/itspacchu/kubeloki/cmd/loki"
+	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
 )
 
 var (
-	PodLogChannels = []kubeapi.PodLogObject{}
+	namespaces = []kubeapi.Namespace{}
 )
 
 func GetKubeDetails(log *log.Logger) error {
 	log.Info("Fetching Kube details")
 	kubeconfig := os.Getenv("HOME") + "/.kube/config" //default kubeconfig path
-	_, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
+	config, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
 	if err != nil {
 		log.Error("Kubeconfig file not found!")
 		return err
 	}
-	log.Info(kubeconfig)
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		log.Error("Unable to create clientset")
+		return err
+	}
+	namespaces = kubeapi.GetNamespaces(clientset)
+
 	if err := loki.PublishLoki(loki.LokiMessage{
 		LogLine:       "Something",
 		Namespace:     "has",
--- cmd/kubeapi/kubeapi.go
+++ cmd/kubeapi/kubeapi.go
@@ -1,7 +1,24 @@
 package kubeapi
 
+import (
+	"time"
+
+	"k8s.io/client-go/kubernetes"
+)
+
 type PodLogObject struct {
 	Name       string
 	Containers []string
 	LogChannel chan string
+}
+
+type Namespace struct {
+	Name           string
+	Pods           []PodLogObject
+	ScrapeInterval time.Duration
+}
+
+func GetNamespaces(clientset *kubernetes.Clientset) []Namespace {
+	
+	return []Namespace{}
 }
